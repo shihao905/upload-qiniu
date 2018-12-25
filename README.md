0ee934e093a12bb8cc0d502236d349ba2a97fcd4
zishu <zishu@anve.com>
2018-12-25 19:00:02 +0800

    update

--- README.md
+++ README.md
@@ -1,24 +1,56 @@
 # anve-upload-qiniu
   
-  -[anve-upload-qiniu](https://github.com/shihao905/upload-qiniu.git)
+  -[https://github.com/shihao905/upload-qiniu.git](https://github.com/shihao905/upload-qiniu.git)
 
-## Install
+## install
 
 ```js
 $ npm install --save anve-upload-qiniu
 ```
 
-
-## Usage
+## use
 
 ```js
 const QiniuUpload = require('./index');
 const path = require('path');
 new QiniuUpload({
-  ACCESS_KEY: 'IxmVzoqgFkFcSQLaAaGkhNh_yrAmZQ1d6cMSMPL3', // your ACCESS_KEY
-  SECRET_KEY: 'KYjmRDbWP_Zk9789MgHvOkWP63pmgWO662ABCTi4v', // your SECRET_KEY
-  bucket: 'ultimavip', // your bucket
-  uploadPath: 'test/', // qiniu files directory
-  fileDirectory: path.resolve(__dirname, './dist') // your file directory
+   /**
+   * ACCESS_KEY
+  */
+  ACCESS_KEY: 'IxmVzoqgaaaFvSQLqAaGkhNh_yrAmZQ1d6cMSMPL3',
+  /**
+   * SECRET_KEY
+  */
+  SECRET_KEY: 'KYjmRDbWP_Zk967MgHvOkWP6sssmgWO662ABCTi4v',
+  /**
+   * 空间名称
+  */
+  bucket: '******',
+  /**
+   * 空间文件目录
+  */
+  uploadPath: '******',
+  /**
+   * 本地文件目录
+  */
+  fileDirectory: path.resolve(__dirname, './dist'),
+  /**
+   * 是否打开上传前的提示 默认打开
+  */
+  openConfirm: false,
+  /**
+   * 上传成功回调
+   * @param {array} files [成功文件列表]
+  */
+  success: function(files) {
+    console.log('success',files)
+  },
+  /**
+   * 上传失败回调
+   * @param {array} files [失败文件列表]
+  */
+  error: function(files){
+    console.log('error',files)
+  }
 });
 ```
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+"use strict";
 const qiniu = require("qiniu");
 const fs = require("fs");
 const path = require("path");
 const fontColor = require("./style");
-const ProgressBar = require('./progress');
+const ProgressBar = require("./progress");
 class QiniuUpload {
   constructor(option) {
     qiniu.conf.ACCESS_KEY = option.ACCESS_KEY || null;
@@ -11,56 +12,100 @@ class QiniuUpload {
     this.uploadPath = option.uploadPath || null;
     this.fileDirectory = option.fileDirectory || null;
     this.token = null;
+    this.loadSuccess = option.success || (()=>{});
+    this.loadError = option.error || (()=>{});
+    if (typeof option.openConfirm == 'boolean') {
+      this.openConfirm = option.openConfirm;
+    } else {
+      this.openConfirm = true;
+    }
     this.filesList = [];
     this.uploadFiles = [];
-    this.init();
+    this.errorFiles = [];
+    this.openConfirm ? this.confirm() : this.init();
+    this.uploading = false;
   }
-  init() {
-    this.getFileList(() => {
+  confirm() {
+    process.stdin.setEncoding('utf8');
+    console.log(fontColor.yellow, `请确认上传信息：`);
+    console.log(fontColor.green, `---ACCESS_KEY：${qiniu.conf.ACCESS_KEY}`);
+    console.log(fontColor.green, `---SECRET_KEY：${qiniu.conf.SECRET_KEY}`);
+    console.log(fontColor.green, `---上传空间：${this.bucket}`);
+    console.log(fontColor.green, `---空间文件目录：${this.uploadPath}`);
+    console.log(fontColor.green, `---本地文件目录：${this.fileDirectory}`);
+    console.log(fontColor.yellow, `确认开始上传吗(N/y)？`);
+    process.stdin.on('data',(input)=>{
+      if (this.uploading) return;
+      input = input.toString().trim();
+      if (['Y', 'y', 'YES', 'yes'].indexOf(input) > -1) {
+        this.uploading = true;
+        this.init(()=>{
+          process.exit();
+          this.uploading = false;
+        });
+      }else {
+        process.exit();
+      }
+    })
+  }
+  init(exit) {
+    this.getFileList((list) => {
+      this.filesList = list;
       if (!this.filesList.length) {
         console.log(fontColor.yellow, "未找到可以上传的文件");
         return;
       }
       console.log(fontColor.yellow, "开始上传...");
-      let pb = new ProgressBar('上传进度');
+      let pb = new ProgressBar("上传进度");
       this.filesList.map(file => {
         file.token = this.getToken(file.key);
         this.uploadFile(file, (err, ret) => {
           if (err) {
             console.log(fontColor.red, "上传失败：" + file.key);
-            console.log(err);
+            this.errorFiles.push(file);
             this.uploadFiles.push(file);
           } else {
             file.url = ret.key;
             this.uploadFiles.push(file);
           }
-          pb.render({ completed: this.uploadFiles.length, total: this.filesList.length });
+          pb.render({
+            completed: this.uploadFiles.length,
+            total: this.filesList.length
+          });
           if (this.uploadFiles.length == this.filesList.length) {
             console.log(fontColor.green, "上传完成！");
+            if (this.errorFiles.length) {
+              console.log(fontColor.red, this.errorFiles.map(res=>(`上传失败：${res.localFile}`)).join('\n'))
+              this.loadError(this.errorFiles);
+            } else {
+              this.loadSuccess(this.uploadFiles);
+            }
+            exit&&exit();
           }
         });
       });
     });
   }
-  getFileList(cb) {
-    let filesList = [];
-    fs.readdir(this.fileDirectory, (err, files) => {
-      if (!err) {
-        files.map(path => {
-          if ( path.charAt(0) !== '.' && fs.statSync(this.fileDirectory + "/" + path).isFile()) {
-            filesList.push({
-              key: (this.uploadPath || "") + path,
-              localFile: (this.fileDirectory || "") + "/" + path
-            });
-          }
-        });
-        this.filesList = filesList;
-        cb&&cb();
-
-      } else {
-        console.log(error);
+  getFileByDir(dirPath, list) {
+    fs.readdirSync(dirPath).map(url => {
+      let u = dirPath + "/" + url;
+      if (url.charAt(0) !== "." && fs.existsSync(u)) {
+        if (fs.statSync(u).isDirectory()) {
+          this.getFileByDir(u, list);
+        } else {
+          list.push({
+            key: this.uploadPath + u.slice(this.fileDirectory.length),
+            localFile: u
+          });
+        }
       }
     });
+  }
+  getFileList(cb) {
+    let filesList = [];
+    this.getFileByDir(this.fileDirectory, filesList);
+    console.log(filesList)
+    cb && cb(filesList);
   }
   getToken(key) {
     let putPolicy = new qiniu.rs.PutPolicy({
